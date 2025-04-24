f0625ae0acd2410d416e74c76ca2c78d5ddc7c90
seunghyonpark <creath.park@gmail.com>
2025-04-24 09:37:42 +0900

    refactor: update request handling to forward headers and body to proxy

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -23,18 +23,70 @@ export default async function handler(req: NextRequest) {
 
   // default proxy to http://52.78.186.199:8080
 
-  const url = new URL(req.url)
-  url.host = 'http://52.78.186.199:8080';
-  url.pathname = url.pathname.replace(/^\/api/, '')
-  return fetch(
-    url.toString(),
-    {
+  try {
+
+    /*
+    const url = new URL(req.url)
+    url.host = 'http://52.78.186.199:8080';
+    url.pathname = url.pathname.replace(/^\/api/, '')
+    return fetch(
+      url.toString(),
+      {
+        method: req.method,
+        headers: req.headers,
+        body: req.body,
+        signal: req.signal,
+      }
+    )
+    */
+
+    // forward the original post request header and body to the http://52.78.186.199:8080
+
+    const url = 'http://52.78.186.199:8080';
+    const response = await fetch(url, {
       method: req.method,
-      headers: req.headers,
+      headers: {
+        'Content-Type': 'application/json',
+        ...Object.fromEntries(req.headers.entries()),
+      },
       body: req.body,
       signal: req.signal,
-    }
-  )
+    })
+    const data = await response.json()
+    const headers = new Headers(response.headers)
+    headers.set('Access-Control-Allow-Origin', '*')
+    headers.set('Access-Control-Allow-Headers', '*')
+    headers.set('Access-Control-Allow-Methods', '*')
+    headers.set('Access-Control-Allow-Credentials', 'true')
+    headers.set('Access-Control-Expose-Headers', '*')
+    headers.set('Content-Type', 'application/json')
+    headers.set('Access-Control-Max-Age', '86400')
+    headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+ 
+
+    return new Response(JSON.stringify(data), {
+      status: response.status,
+      headers: headers,
+    })
+    
+    
+
+  } catch (e) {
+    console.log(e)
+    return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Expose-Headers': '*',
+        'Access-Control-Max-Age': '86400',
+      },
+    })
+  }
 
 
 
